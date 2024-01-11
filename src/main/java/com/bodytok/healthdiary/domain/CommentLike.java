c4685fb3e61dfa6370dc19087fe18fd3435e6841
twonezero <dnjsdudtm98@naver.com>
2024-01-11 14:55:33 +0900

    #3 -도메인 클래스 설계
    erd 를 참고하여 java 클래스로 도메인을 작성하였다.

---
+++ src/main/java/com/bodytok/healthdiary/domain/CommentLike.java
@@ -0,0 +1,12 @@
+package com.bodytok.healthdiary.domain;
+
+
+public class CommentLike {
+
+    private Long id;
+
+    private UserAccount userAccount;
+
+    private Comment comment;
+
+}
---
+++ src/main/java/com/bodytok/healthdiary/domain/DiaryLike.java
@@ -0,0 +1,11 @@
+package com.bodytok.healthdiary.domain;
+
+
+public class DiaryLike {
+
+    private Long id;
+    private UserAccount userAccount;
+
+    private PersonalExerciseDiary personalExerciseDiary;
+
+}
