920a6d7bce7331c885e99e8a0628b3c5db2b7d55
twonezero <dnjsdudtm98@naver.com>
2024-01-19 18:54:45 +0900

    #5 - repository 인터페이스 작성
    db 입출력을 위한 레포지토리 레이어를 생성하고 각 도메인별로 작성함.
    `@RepositoryRestResource` 어노테이션을 달아 data rest 가 작동하도록 함.

---
+++ src/main/java/com/bodytok/healthdiary/repository/UserAccountRepository.java
@@ -0,0 +1,10 @@
+package com.bodytok.healthdiary.repository;
+
+import com.bodytok.healthdiary.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
