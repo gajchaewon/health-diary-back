f903c69a265162ef72c53e2f8c80840291fd1126
twonezero <dnjsdudtm98@naver.com>
2024-01-19 18:51:19 +0900

    #5 - jpaConfig 설정, Auditing 설정
    각 도메인의 시간관련 메타데이터 필드를 auditing 기능을 통해 정리하였음.

---
+++ src/main/java/com/bodytok/healthdiary/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.bodytok.healthdiary.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware () {
+        return () -> Optional.of("ilgong"); // TODO : 스프링 시큐리티 설정 시 변경
+    }
+}
