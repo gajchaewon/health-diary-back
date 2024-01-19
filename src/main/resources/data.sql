b8021d18145c155af752a9c68ec49d062fdd1963
twonezero <dnjsdudtm98@naver.com>
2024-01-19 18:56:09 +0900

    #5 - data.sql 초기 데이터 script 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+
+-- UserAccount 테이블에 대한 테스트 데이터
+INSERT INTO user_account (user_id, email, user_password, nickname, created_at, modified_at)
+VALUES (1,'test1@gmail.com', 'password1', 'nickname1', NOW(), NOW()),
+       (2,'test2@gmail.com', 'password2', 'nickname2', NOW(), NOW());
+
+-- PersonalExerciseDiary 테이블에 대한 테스트 데이터
+INSERT INTO personal_exercise_diary (diary_id, user_id, title, content, total_ex_time, is_public, youtube_url, created_at,
+                                   modified_at)
+VALUES (1, 1, 'title1', 'content1', 30, true, 'youtubeUrl1', NOW(), NOW()),
+       (2, 2, 'title2', 'content2', 60, false, 'youtubeUrl2', NOW(), NOW()),
+       (3, 1, 'title3', 'content3', 45, true, 'youtubeUrl3', NOW(), NOW()),
+       (4, 1, 'title4', 'content4', 90, false, 'youtubeUrl4', NOW(), NOW()),
+       (5, 1, 'title5', 'content5', 120, true, 'youtubeUrl5', NOW(), NOW()),
+       (6, 2, 'title6', 'content6', 150, false, 'youtubeUrl6', NOW(), NOW()),
+       (7, 2, 'title7', 'content7', 180, true, 'youtubeUrl7', NOW(), NOW()),
+       (8, 2, 'title8', 'content8', 210, false, 'youtubeUrl8', NOW(), NOW()),
+       (9, 2, 'title9', 'content9', 240, true, 'youtubeUrl9', NOW(), NOW()),
+       (10, 2, 'title10', 'content10', 270, false, 'youtubeUrl10', NOW(), NOW());
