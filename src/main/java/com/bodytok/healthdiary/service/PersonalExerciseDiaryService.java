85e580e5fbc13589f3e6c1cff74263a781d06cc9
twonezero <dnjsdudtm98@naver.com>
2024-01-30 11:54:31 +0900

    #13 - 다이어리 서비스 로직 정의 및 구현
    간단한 crud  로직과 테스트 코드를 작성함
    TODO
    * 저장 시 커뮤니티에 동시 저장
    * 업데이트 시 해시태그에 대한 내용도 업데이트 하는 로직 구현 필요

---
+++ src/main/java/com/bodytok/healthdiary/service/PersonalExerciseDiaryService.java
@@ -0,0 +1,57 @@
+package com.bodytok.healthdiary.service;
+
+
+import com.bodytok.healthdiary.domain.PersonalExerciseDiary;
+import com.bodytok.healthdiary.domain.UserAccount;
+import com.bodytok.healthdiary.dto.PersonalExerciseDiaryDto;
+import com.bodytok.healthdiary.repository.PersonalExerciseDiaryRepository;
+import com.bodytok.healthdiary.repository.UserAccountRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class PersonalExerciseDiaryService {
+
+    private final PersonalExerciseDiaryRepository diaryRepository;
+    private final UserAccountRepository userAccountRepository;
+    @Transactional(readOnly = true)
+    public Page<PersonalExerciseDiaryDto> getAllDiaries(Pageable pageable) {
+        return diaryRepository.findAll(pageable).map(PersonalExerciseDiaryDto::from);
+    }
+
+    @Transactional(readOnly = true)
+    public PersonalExerciseDiaryDto getDiary(Long diaryId) {
+        return diaryRepository.findById(diaryId)
+                .map(PersonalExerciseDiaryDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("다이어리가 없습니다. - diaryId : "+ diaryId));
+    }
+
+
+    public void saveDiary(PersonalExerciseDiaryDto dto) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().id());
+        diaryRepository.save(dto.toEntity(userAccount));
+    }
+
+    public void updateDiary(PersonalExerciseDiaryDto dto) {
+        try {
+            PersonalExerciseDiary diary = diaryRepository.getReferenceById(dto.id());
+            if (dto.title() != null) { diary.setTitle(dto.title()); }
+            if (dto.content() != null) { diary.setContent(dto.content()); }
+            //TODO : 해시태그 업데이트 구현
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
+    }
+
+    public void deleteDiary(Long diaryId) {
+        diaryRepository.deleteById(diaryId);
+    }
+}
