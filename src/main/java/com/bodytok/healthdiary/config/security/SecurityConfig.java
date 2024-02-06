8e4a9f462fd4879e4af45ccd1c1c1e67ff226dce
twonezero <dnjsdudtm98@naver.com>
2024-02-06 15:42:19 +0900

    #21 - Security Config 수정 및 예외 처리 entry point 추가

--- src/main/java/com/bodytok/healthdiary/config/security/SecurityConfig.java
+++ src/main/java/com/bodytok/healthdiary/config/security/SecurityConfig.java
@@ -3,24 +3,33 @@ package com.bodytok.healthdiary.config.security;
 
 import com.bodytok.healthdiary.filter.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthFilter, AuthenticationProvider authenticationProvider, CustomAuthenticationEntryPoint authenticationEntryPoint) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.authenticationProvider = authenticationProvider;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -31,9 +40,16 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         auth -> auth
-                                .requestMatchers("/diaries").permitAll()
+                                .requestMatchers("auth/login/**").permitAll()
+                                .requestMatchers("auth/sign-up/**").permitAll()
+                                .requestMatchers(HttpMethod.GET,"diaries").permitAll()
                                 .anyRequest().authenticated()
                 )
+                //Authentication Entry Point -> Exception Handler
+                .exceptionHandling(
+                        config -> config.authenticationEntryPoint(authenticationEntryPoint)
+                )
+                // Set Session policy = STATELESS
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
