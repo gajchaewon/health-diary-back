d0f52f98106774518d20e46ff440ca5e7eb27872
twonezero <dnjsdudtm98@naver.com>
2024-02-29 18:17:12 +0900

    #44 - 좋아요 도메인 정의 및 관계 매핑
    개인 운동일지에 양방향 매핑 필드를 구성한다.
    * TODO : 저장 로직 등 추가할 것이 많음.

--- src/main/java/com/bodytok/healthdiary/domain/DiaryLike.java
+++ src/main/java/com/bodytok/healthdiary/domain/DiaryLike.java
@@ -1,11 +1,57 @@
 package com.bodytok.healthdiary.domain;
 
 
-public class DiaryLike {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.util.Objects;
+
+
+@Getter
+@ToString(callSuper = true)
+@Table(indexes = {
+        @Index(columnList = "createdAt"),
+//        @Index(columnList = "user_id"),
+//        @Index(columnList = "diary_id") -> 추후 쿼리 작성 시 고려
+})
+@Entity
+public class DiaryLike extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
     private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private UserAccount userAccount;
 
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "diary_id", nullable = false)
     private PersonalExerciseDiary personalExerciseDiary;
 
+    protected DiaryLike() {
+    }
+
+    private DiaryLike(UserAccount userAccount, PersonalExerciseDiary personalExerciseDiary) {
+        this.userAccount = userAccount;
+        this.personalExerciseDiary = personalExerciseDiary;
+    }
+
+    public static DiaryLike of(UserAccount userAccount, PersonalExerciseDiary personalExerciseDiary) {
+        return new DiaryLike(userAccount, personalExerciseDiary);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DiaryLike that)) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
