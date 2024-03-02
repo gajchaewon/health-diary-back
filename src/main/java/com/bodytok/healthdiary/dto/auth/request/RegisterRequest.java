5d1b5b46d26144f40e43e97f2d56832b43885d2c
twonezero <dnjsdudtm98@naver.com>
2024-03-02 14:16:55 +0900

    #50 - refactoring : 회원가입 시 프로필 입력 제거, 관련된 코드 수정

--- src/main/java/com/bodytok/healthdiary/dto/auth/request/RegisterRequest.java
+++ src/main/java/com/bodytok/healthdiary/dto/auth/request/RegisterRequest.java
@@ -1,17 +1,11 @@
 package com.bodytok.healthdiary.dto.auth.request;
 
 
-import lombok.*;
 
 
-@ToString
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-public class RegisterRequest {
-    private String email;
-    private String password;
-    private String nickname;
-    private Byte[] profileImage;
+public record RegisterRequest(
+        String email,
+        String password,
+        String nickname
+) {
 }
