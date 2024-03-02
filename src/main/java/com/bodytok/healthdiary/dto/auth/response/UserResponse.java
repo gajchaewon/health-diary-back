7df70a1bfb6e4f2b7372c6621af2e32eb3df9b89
twonezero <dnjsdudtm98@naver.com>
2024-03-02 14:17:42 +0900

    #50 - refactoring : 유저 반환 정보 중 닉네임이 없을 시 이메일을 닉네임으로 반환

--- src/main/java/com/bodytok/healthdiary/dto/auth/response/UserResponse.java
+++ src/main/java/com/bodytok/healthdiary/dto/auth/response/UserResponse.java
@@ -2,19 +2,31 @@ package com.bodytok.healthdiary.dto.auth.response;
 
 import com.bodytok.healthdiary.dto.UserAccountDto;
 
+import java.time.LocalDateTime;
+
 public record UserResponse(
         Long id,
         String email,
         String nickname,
-        Byte[] profileImage
+        Byte[] profileImage,
+        LocalDateTime createdAt,
+        LocalDateTime modifiedAt
 ) {
 
     public static UserResponse from(UserAccountDto dto){
+        String nickname = dto.nickname();
+        if (nickname == null || nickname.isBlank()) {
+           nickname = dto.nickname();
+        }
+
         return new UserResponse(
                 dto.id(),
                 dto.email(),
-                dto.nickname(),
-                dto.profileImage()
+                nickname,
+                dto.profileImage(),
+                dto.createdAt(),
+                dto.modifiedAt()
         );
     }
+
 }
