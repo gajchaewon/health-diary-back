8eda2fcd63aa3711bb0d644f886614c5b505aade
twonezero <dnjsdudtm98@naver.com>
2024-03-16 17:03:21 +0900

    #62 - Swagger dto 스키마 설정

--- src/main/java/com/bodytok/healthdiary/dto/auth/request/AuthenticationRequest.java
+++ src/main/java/com/bodytok/healthdiary/dto/auth/request/AuthenticationRequest.java
@@ -1,7 +1,12 @@
 package com.bodytok.healthdiary.dto.auth.request;
 
-import lombok.*;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record AuthenticationRequest(String email, String password) {
+public record AuthenticationRequest(
+        @Schema(example = "superman@mail.com")
+        String email,
+
+        @Schema(example = "superman1234!")
+        String password) {
 }
--- src/main/java/com/bodytok/healthdiary/dto/diary/request/DiaryRequest.java
+++ src/main/java/com/bodytok/healthdiary/dto/diary/request/DiaryRequest.java
@@ -2,13 +2,19 @@ package com.bodytok.healthdiary.dto.diary.request;
 
 import com.bodytok.healthdiary.dto.UserAccountDto;
 import com.bodytok.healthdiary.dto.diary.DiaryDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.Set;
 
+@Schema(description = "다이어리 POST, PUT 요청 바디")
 public record DiaryRequest(
+    @Schema(description = "제목", nullable = false, example = "멋진 내 다이어리")
     String title,
+    @Schema(description = "내용", nullable = false, example = "컨텐츠 내용 - 오늘은 맛있는 커피를 마셨다.")
     String content,
+    @Schema(description = "커뮤니티 공개 여부", nullable = false, example = "false")
     Boolean isPublic,
+    @Schema(description = "해시태그", nullable = true)
     Set<String> hashtags
 ) {
     public static DiaryRequest of(String title, String content, Boolean isPublic, Set<String> hashtags) {
