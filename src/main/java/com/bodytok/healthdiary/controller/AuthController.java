68b5ee38679e50b21742802745f5688eef417bb5
twonezero <dnjsdudtm98@naver.com>
2024-03-20 15:15:24 +0900

    #68 - refreshToken 관련 로직 생성 및 컨트롤러 수정
    refreshToken의 유효성을 체크하고, 쿠키로 담아 return 하는 로직을 만들고,
    controller 에서 response.cookie 에 담는다.

--- src/main/java/com/bodytok/healthdiary/controller/AuthController.java
+++ src/main/java/com/bodytok/healthdiary/controller/AuthController.java
@@ -1,20 +1,32 @@
 package com.bodytok.healthdiary.controller;
 
 
+import com.bodytok.healthdiary.domain.security.CustomUserDetails;
 import com.bodytok.healthdiary.dto.auth.request.AuthenticationRequest;
 import com.bodytok.healthdiary.dto.auth.request.RegisterRequest;
 import com.bodytok.healthdiary.dto.auth.response.LoginResponse;
 import com.bodytok.healthdiary.dto.auth.response.RefreshTokenResponse;
 import com.bodytok.healthdiary.dto.auth.response.RegisterResponse;
 import com.bodytok.healthdiary.service.auth.AuthenticationService;
+import com.bodytok.healthdiary.service.jwt.JwtService;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 
 @RequiredArgsConstructor
@@ -24,12 +36,17 @@ import java.util.Objects;
 public class AuthController {
 
     private final AuthenticationService authService;
-
+    private final JwtService jwtService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@RequestBody AuthenticationRequest request) {
-        LoginResponse response = authService.authenticate(request);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<LoginResponse> login(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
+        LoginResponse loginResponse = authService.authenticate(request);
+
+        var refreshToken = loginResponse.tokenResponse().refreshToken();
+        // refreshToken을 쿠키에 담아서 반환
+        Cookie refreshTokenCookie = jwtService.createCookie(refreshToken);
+        response.addCookie(refreshTokenCookie);
+        return ResponseEntity.ok(loginResponse);
     }
 
     @PostMapping("/sign-up")
@@ -38,16 +55,17 @@ public class AuthController {
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/refresh-token")
+    @GetMapping("/refresh-token")
     public ResponseEntity<RefreshTokenResponse> refreshToken(
-            @CookieValue(value = "refreshToken", required = false) String refreshToken
-    ) throws IOException {
+            @CookieValue(value = "refreshToken") String refreshToken
+            ) throws IOException {
         if (refreshToken == null)
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(RefreshTokenResponse.of(null,"REFRESH_TOKEN_FAILED"));
-        String newRefreshToken = authService.refreshToken(refreshToken);
-        if (Objects.equals(newRefreshToken, "REFRESH_TOKEN_FAILED")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(RefreshTokenResponse.of(null,"REFRESH_TOKEN_NULL"));
+
+        String newAccessToken = authService.refreshToken(refreshToken);
+        if (Objects.equals(newAccessToken, "REFRESH_TOKEN_FAILED")) {
             return ResponseEntity.status(HttpStatus.FORBIDDEN).body(RefreshTokenResponse.of(null,"REFRESH_TOKEN_FAILED"));
         }
-        return ResponseEntity.ok(RefreshTokenResponse.of(newRefreshToken, "REFRESH_TOKEN_SUCCESS"));
+        return ResponseEntity.ok(RefreshTokenResponse.of(newAccessToken, "REFRESH_TOKEN_SUCCESS"));
     }
 }
