41712f95cee0e4e4940fc1301bb3cb973d8efabe
twonezero <dnjsdudtm98@naver.com>
2024-04-18 15:48:11 +0900

    #110 - jpa 레포지토리 스캔 클래스 지정
    * redis repository와 jpa repository  가 서로 스캔하는 걸 방지하기 위해 지정했음.

--- src/main/java/com/bodytok/healthdiary/HealthDiaryApplication.java
+++ src/main/java/com/bodytok/healthdiary/HealthDiaryApplication.java
@@ -1,8 +1,15 @@
 package com.bodytok.healthdiary;
 
+import com.bodytok.healthdiary.repository.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackageClasses = {
+		CommentRepository.class, DiaryImageRepository.class, DiaryLikeRepository.class, ExerciseTimeLogRepository.class,
+		FollowRepository.class, HashtagRepository.class, PersonalExerciseDiaryHashtagRepository.class, PersonalExerciseDiaryRepository.class,
+		UserAccountRepository.class
+})
 @SpringBootApplication
 public class HealthDiaryApplication {
 
