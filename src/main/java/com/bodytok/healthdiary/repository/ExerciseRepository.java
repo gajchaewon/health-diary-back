0d33da59497f8be33e2e684e98a8554dcb997c98
twonezero <dnjsdudtm98@naver.com>
2024-05-24 13:24:42 +0900

    #121 - fix : ` QuerydslBinderCustomizer` 의 customize() 함수 구현

--- src/main/java/com/bodytok/healthdiary/repository/ExerciseRepository.java
+++ src/main/java/com/bodytok/healthdiary/repository/ExerciseRepository.java
@@ -2,12 +2,23 @@ package com.bodytok.healthdiary.repository;
 
 import com.bodytok.healthdiary.domain.Exercise;
 import com.bodytok.healthdiary.domain.QExercise;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 public interface ExerciseRepository extends
         JpaRepository<Exercise, Long>,
         QuerydslPredicateExecutor<Exercise>,
-        QuerydslBinderCustomizer<QExercise> {
+        QuerydslBinderCustomizer<QExercise>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, QExercise root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.exerciseName,root.description);
+        bindings.bind(root.exerciseName).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.description).first(StringExpression::containsIgnoreCase);
+    }
 }
