7abd4729d9c31562e81972fcc9c3bca997e36ce3
twonezero <dnjsdudtm98@naver.com>
2024-06-18 18:09:13 +0900

    #155 - @EnableJpaRepositories로 인한 WebMvcTest 에러 수정
    `@EnableJpaRepositories` 가 스캔대상이 되어, 계속해서 EntityManageFacotry 에 대한 의존성 주입 오류가 발생하였다. JpaConfig 로 이동해서 해결하였음.
    
    * test용 lombok 의존성 설치
    * 필요없는 uitl 삭제

--- src/main/java/com/bodytok/healthdiary/HealthDiaryApplication.java
+++ src/main/java/com/bodytok/healthdiary/HealthDiaryApplication.java
@@ -1,18 +1,10 @@
 package com.bodytok.healthdiary;
 
-import com.bodytok.healthdiary.repository.jwt.JwtTokenRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@EnableJpaRepositories(basePackageClasses = {
-//		CommentRepository.class, DiaryImageRepository.class, DiaryLikeRepository.class, ExerciseTimeLogRepository.class,
-//		FollowRepository.class, HashtagRepository.class, PersonalExerciseDiaryHashtagRepository.class, PersonalExerciseDiaryRepository.class,
-//		UserAccountRepository.class
-//})
-@EnableJpaRepositories(basePackages = "com.bodytok.healthdiary.repository", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = JwtTokenRepository.class))
+
+
 @SpringBootApplication
 public class HealthDiaryApplication {
 
