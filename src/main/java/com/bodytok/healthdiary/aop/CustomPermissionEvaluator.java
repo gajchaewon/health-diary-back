da635194fe54941cb34db59d408668f12a583fdc
twonezero <dnjsdudtm98@naver.com>
2024-06-22 15:27:20 +0900

    #157 - 루틴에 @PreAuthorize 로직 작성

--- src/main/java/com/bodytok/healthdiary/aop/CustomPermissionEvaluator.java
+++ src/main/java/com/bodytok/healthdiary/aop/CustomPermissionEvaluator.java
@@ -2,12 +2,12 @@ package com.bodytok.healthdiary.aop;
 
 import com.bodytok.healthdiary.domain.Comment;
 import com.bodytok.healthdiary.domain.PersonalExerciseDiary;
+import com.bodytok.healthdiary.domain.Routine;
 import com.bodytok.healthdiary.domain.security.CustomUserDetails;
 import com.bodytok.healthdiary.exepction.CustomBaseException;
-import com.bodytok.healthdiary.exepction.CustomError;
 import com.bodytok.healthdiary.repository.CommentRepository;
 import com.bodytok.healthdiary.repository.PersonalExerciseDiaryRepository;
-import com.bodytok.healthdiary.repository.UserAccountRepository;
+import com.bodytok.healthdiary.repository.RoutineRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.PermissionEvaluator;
@@ -22,6 +22,7 @@ import static com.bodytok.healthdiary.exepction.CustomError.*;
 @RequiredArgsConstructor
 @Component
 public class CustomPermissionEvaluator implements PermissionEvaluator {
+    private final RoutineRepository routineRepository;
     private final PersonalExerciseDiaryRepository diaryRepository;
     private final CommentRepository commentRepository;
 
@@ -35,7 +36,6 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
         CustomUserDetails userPrincipal = (CustomUserDetails) authentication.getPrincipal();
         TargetType type = TargetType.fromString(targetType);
         log.info(">>>type: {}",type);
-        log.info(">>>targetId: {}",targetId);
         return switch (type) {
             case DIARY -> {
                 PersonalExerciseDiary diary = diaryRepository.findById((Long) targetId)
@@ -47,6 +47,11 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
                         .orElseThrow(() -> new CustomBaseException(COMMENT_NOT_FOUND));
                 yield comment.getUserAccount().getId().equals(userPrincipal.getId());
             }
+            case ROUTINE -> {
+                Routine routine = routineRepository.findById((Long) targetId)
+                        .orElseThrow(() -> new CustomBaseException(ROUTINE_NOT_FOUND));
+                yield  routine.getUserAccount().getId().equals(userPrincipal.getId());
+            }
             default -> {
                 log.error("[인가 실패] 사용자 소유가 아닙니다.");
                 yield false;
@@ -56,7 +61,8 @@ public class CustomPermissionEvaluator implements PermissionEvaluator {
 
     public enum TargetType {
         DIARY,
-        COMMENT;
+        COMMENT,
+        ROUTINE;
 
         public static TargetType fromString(String targetType) {
             try {
--- src/main/java/com/bodytok/healthdiary/controller/ExerciseRoutineController.java
+++ src/main/java/com/bodytok/healthdiary/controller/ExerciseRoutineController.java
@@ -8,9 +8,13 @@ import com.bodytok.healthdiary.dto.exercise_routine.request.RoutineCreate;
 import com.bodytok.healthdiary.dto.exercise_routine.request.RoutineUpdate;
 import com.bodytok.healthdiary.dto.exercise_routine.response.RoutineWithExerciseResponse;
 import com.bodytok.healthdiary.service.ExerciseRoutineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,8 +29,10 @@ public class ExerciseRoutineController {
     private final ExerciseRoutineService exRoutineService;
 
     @GetMapping
+    @Operation(summary = "유저 또는 나의 모든 루틴 조회")
     public ResponseEntity<List<RoutineWithExerciseResponse>> getAllRoutine(
             @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Parameter(name = "userId",description = "루틴 조회 유저 ID", allowEmptyValue = true)
             @RequestParam(name = "userId", required = false) Long userId
     ){
         /**
@@ -40,37 +46,32 @@ public class ExerciseRoutineController {
     }
 
     @PostMapping
+    @Operation(summary = "내 루틴 생성")
     public ResponseEntity<RoutineWithExerciseResponse> saveRoutine(
             @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody RoutineCreate routineCreate
+            @RequestBody @Valid RoutineCreate routineCreate
     ) {
         var toDto = routineMapper.toDtoFromCreate(routineCreate);
         RoutineDto dto = exRoutineService.saveRoutine(toDto, userDetails.getId());
         return ResponseEntity.ok().body(RoutineWithExerciseResponse.from(dto));
     }
 
-    @PostMapping("/exercise")
-    public ResponseEntity<RoutineWithExerciseResponse> saveExercise(
-            @AuthenticationPrincipal CustomUserDetails userDetails,
-            @RequestBody ExerciseCreate exerciseCreate) {
-        var toDto = exerciseCreate.toRoutineDto();
-        RoutineDto routineDto = exRoutineService.saveExercise(toDto);
-        return ResponseEntity.ok().body(RoutineWithExerciseResponse.from(routineDto));
-    }
-
+    @PreAuthorize("hasPermission(#routineId, 'ROUTINE', 'UPDATE')")
     @PutMapping("/{routineId}")
+    @Operation(summary = "내 루틴 업데이트")
     public ResponseEntity<RoutineWithExerciseResponse> updateRoutine(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable(name = "routineId") Long routineId,
-            @RequestBody RoutineUpdate routineUpdate
+            @RequestBody @Valid RoutineUpdate routineUpdate
     ){
         var toDto = routineMapper.toDtoFromUpdate(routineUpdate, routineId, userDetails.toDto());
         RoutineDto routineDto = exRoutineService.updateRoutine(toDto);
         return ResponseEntity.ok().body(RoutineWithExerciseResponse.from(routineDto));
     }
 
-
+    @PreAuthorize("hasPermission(#routineId, 'ROUTINE', 'DELETE')")
     @DeleteMapping("/{routineId}")
+    @Operation(summary = "내 루틴 삭제")
     public ResponseEntity<Void> deleteRoutine(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable(name = "routineId") Long routineId
@@ -80,7 +81,21 @@ public class ExerciseRoutineController {
         return ResponseEntity.noContent().build();
     }
 
+
+    @PreAuthorize("hasPermission(#routineId, 'ROUTINE', 'UPDATE')")
+    @PostMapping("/{routineId}/exercise")
+    @Operation(summary = "내 운동 생성 (루틴 업데이트)")
+    public ResponseEntity<RoutineWithExerciseResponse> saveExercise(
+            @PathVariable(name = "routineId") Long routineId,
+            @RequestBody @Valid ExerciseCreate exerciseCreate) {
+        var toDto = exerciseCreate.toRoutineDto(routineId);
+        RoutineDto routineDto = exRoutineService.saveExercise(toDto);
+        return ResponseEntity.ok().body(RoutineWithExerciseResponse.from(routineDto));
+    }
+
+    @PreAuthorize("hasPermission(#routineId, 'ROUTINE', 'UPDATE')")
     @DeleteMapping("{routineId}/exercise/{exerciseId}")
+    @Operation(summary = "내 운동 삭제 (루틴 업데이트)")
     public ResponseEntity<RoutineWithExerciseResponse> deleteExercise(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable(name = "routineId") Long routineId,
